eb757a6cb31c175e9d5e90d27eaabffe4ae29852
Adam Dimmick <66253578+awdimmick@users.noreply.github.com>
2025-03-31 20:34:22 +0100

    Add files via upload

---
+++ OPEN DAY - cipher.py
@@ -0,0 +1,21 @@
+def letter_to_number(letter):
+    return ord(letter) - 64
+
+def number_to_letter(number):
+    return chr(number + 64)
+
+def shift_letter(letter, key):
+    shiftedNum = letter_to_number(letter) + key
+    
+    if shiftedNum < 1:
+        shiftedNum = shiftedNum + 26
+    elif shiftedNum > 26:
+        shiftedNum = shiftedNum - 26
+
+    return number_to_letter(shiftedNum)
+
+# Add the code to make the cipher work below
+
+
+
+
